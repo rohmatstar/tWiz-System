ef788eacf3fb40fc05e718f9e3793f730482ffa2
Febrianto752 <febrianto.bekasi@gmail.com>
2023-07-24 10:29:16 +0700

    feat: add account roles dto

---
+++ API/DTOs/AccountRoles/NewAccountRoleDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs.AccountRoles
+{
+    public class NewAccountRoleDto
+    {
+        [Required]
+        public Guid AccountGuid { get; set; }
+        [Required]
+        public Guid RoleGuid { get; set; }
+    }
+}
---
+++ API/DTOs/AccountRoles/UpdateAccountRoleDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs.AccountRoles
+{
+    public class UpdateAccountRoleDto
+    {
+        [Required]
+        public Guid Guid { get; set; }
+        [Required]
+        public Guid AccountGuid { get; set; }
+        [Required]
+        public Guid RoleGuid { get; set; }
+    }
+}
