2f39b189629cd1e1b2b0333668d7cfd9e8073b43
Febrianto752 <febrianto.bekasi@gmail.com>
2023-07-24 10:26:48 +0700

    feat: add roles dtos

---
+++ API/DTOs/Roles/NewRoleDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs.Roles
+{
+    public class NewRoleDto
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ API/DTOs/Roles/UpdateRoleDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs.Roles
+{
+    public class UpdateRoleDto
+    {
+        [Required]
+        public Guid Guid { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
