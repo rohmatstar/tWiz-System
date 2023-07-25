4cfba429d1f5f45331bd030fe94630862c08d543
Febrianto752 <febrianto.bekasi@gmail.com>
2023-07-25 11:03:22 +0700

    feat: add validation dtos
    Add validation in account, account role, bank, and company dtos

--- API/DTOs/Banks/UpdateBankDto.cs
+++ API/DTOs/Banks/UpdateBankDto.cs
@@ -1,16 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace API.DTOs.Banks
+namespace API.DTOs.Banks;
+
+public class UpdateBankDto
 {
-    public class UpdateBankDto
-    {
-        [Required]
-        public Guid Guid { get; set; }
+    [Required]
+    public Guid Guid { get; set; }
 
-        [Required]
-        public string Code { get; set; }
+    [Required]
+    public string Code { get; set; }
 
-        [Required]
-        public string Name { get; set; }
-    }
+    [Required]
+    public string Name { get; set; }
 }
+
