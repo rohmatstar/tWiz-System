2c636d31b50c6aab28c64af2f1fa42d97393fd5e
AHN-YU-JIN\Ahn Yu Jin <dev.rohmatstar@gmail.com>
2023-07-25 13:47:02 +0700

    Landing Page

--- Client/Controllers/HomeController.cs
+++ Client/Controllers/HomeController.cs
@@ -18,7 +18,12 @@ namespace Client.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
         {
             return View();
         }
