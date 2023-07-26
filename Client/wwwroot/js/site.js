88a29c579cb4dd137d4b322c67ab7c7a5104ec00
AHN-YU-JIN\Ahn Yu Jin <dev.rohmatstar@gmail.com>
2023-07-26 13:48:38 +0700

    Reset Password

--- Client/wwwroot/js/site.js
+++ Client/wwwroot/js/site.js
@@ -148,3 +148,132 @@ $('#register-btn').click(function (event) {
         }
     });
 })
+$('#forgot-btn').click(function (e) {
+    console.log($("#email-reset").val())
+    e.preventDefault();
+    let btn_text = $("#forgot-btn").html();
+    $('#forgot-btn').html(`<div class="spinner-border text-light" role="status"><span class="sr-only">Loading...</span></div>`)
+
+    var data = {
+        email: $('#email-reset').val()
+    };
+
+    $.ajax({
+        method: 'POST',
+        url: 'https://localhost:7249/api/auths/forgot-password?email=' + data.email,
+        //data: JSON.stringify(data),
+        contentType: 'application/json',
+        success: function (response) {
+            console.log(response);
+            $("#error-list").html("")
+
+            if (response.code == 200) {
+                document.location.href = "ConfirmEmail?email=" + data.email;
+            }
+            $("#forgot-btn").html(btn_text)
+        },
+        error: function (xhr, status, error) {
+            let error_item = "";
+            $("#error-list").html("")
+
+            var errorResponse = xhr.responseJSON;
+
+            if (errorResponse && errorResponse.errors) {
+                console.log("xhr", xhr.responseJSON.errors);
+
+                var emailErrors = errorResponse.errors.email;
+
+
+                if (emailErrors) {
+                    error_item += "<br/>";
+                    error_item += emailErrors.join('<br/>');
+                }
+            }
+
+
+            $("#error-list").append(error_item)
+            $("#forgot-btn").html(btn_text)
+        }
+    });
+})
+
+$('#reset-btn').click(function (e) {
+    const url = window.location.href;
+    function getQueryParam(url, param) {
+        const searchParams = new URL(url).searchParams;
+        return searchParams.get(param);
+    }
+
+    const email = getQueryParam(url, 'email');
+    e.preventDefault();
+    let btn_text = $("#reset-btn").html();
+    $('#reset-btn').html(`<div class="spinner-border text-light" role="status"><span class="sr-only">Loading...</span></div>`)
+
+    var data = {
+        email: email,
+        token: $("#token").val(),
+        NewPassword: $("#new-password").val(),
+        ConfirmPassword: $("#confirm-password").val()
+    };
+
+    $.ajax({
+        method: 'PUT',
+        url: 'https://localhost:7249/api/auths/change-password',
+        data: JSON.stringify(data),
+        contentType: 'application/json',
+        success: function (response) {
+            console.log("res", response);
+            $("#error-list").html("")
+
+            if (response.code == 200) {
+                alert("Password Changed successfully")
+                document.location.href = "Login";
+            }
+            else {
+                $("#error-list").html("Oops Something went wrong!")
+            }
+            $("#reset-btn").html(btn_text)
+        },
+        error: function (xhr, status, error) {
+            let error_item = "";
+            $("#error-list").html("")
+
+            var errorResponse = xhr.responseJSON;
+
+            if (errorResponse && errorResponse.errors) {
+                console.log("xhr", xhr.responseJSON.errors);
+                console.log("status", status)
+                console.log("error", error)
+
+                var confirmPasswordErrors = errorResponse.errors.ConfirmPassword;
+                var emailErrors = errorResponse.errors.Email;
+                var passwordErrors = errorResponse.errors.NewPassword;
+                var DtoError = errorResponse.errors.changePasswordDto;
+
+                if (confirmPasswordErrors) {
+                    error_item += "<br/>";
+                    error_item += confirmPasswordErrors.join('<br/>');
+                }
+
+                if (emailErrors) {
+                    error_item += "<br/>";
+                    error_item += emailErrors.join('<br/>');
+                }
+
+                if (passwordErrors) {
+                    error_item += "<br/>";
+                    error_item += passwordErrors.join('<br/>');
+                }
+
+                if (DtoError) {
+                    error_item += "<br/>";
+                    error_item += DtoError.join('<br/>');
+                }
+            }
+
+
+            $("#error-list").append(error_item)
+            $("#reset-btn").html(btn_text)
+        }
+    });
+})
