bbd48cfed7c190d1f6a399d15c4d11a9b959a70a
Febrianto752 <febrianto.bekasi@gmail.com>
2023-08-01 06:02:32 +0700

    feat: add dto in events dto
    Create GetEventDto and QueryParamGetEventDto

---
+++ API/DTOs/Events/GetEventDto.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs.Events
+{
+    public class GetEventDto
+    {
+        public Guid Guid { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string? Thumbnail { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Type { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        public string Status { get; set; }
+
+        [Required]
+        public string Payment { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int Quota { get; set; }
+        public int Joined { get; set; }
+
+        [Required]
+        public string StartDate { get; set; }
+
+        [Required]
+        public string EndDate { get; set; }
+
+        [Required]
+        public Guid Organizer { get; set; }
+
+        [Required]
+        public string Place { get; set; }
+
+    }
+}
---
+++ API/DTOs/Events/QueryParamGetEventDto.cs
@@ -0,0 +1,17 @@
+﻿namespace API.DTOs.Events;
+
+public class QueryParamGetEventDto
+{
+    // all, published, draft
+    public string? Publication { get; set; }
+
+    // all, private, public
+    public string? Visibility { get; set; }
+
+    // all, offline, online
+    public string? Type { get; set; }
+
+    // newest, older
+    public string? SortBy { get; set; }
+}
+
