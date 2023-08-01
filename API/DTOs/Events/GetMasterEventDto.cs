8e6a2a30e38881eaf5b491766c00999d90171d5b
Febrianto752 <febrianto.bekasi@gmail.com>
2023-08-02 06:09:29 +0700

    fix: update get event master dto

--- API/DTOs/Events/GetMasterEventDto.cs
+++ API/DTOs/Events/GetMasterEventDto.cs
@@ -1,60 +1,56 @@
 ﻿using API.DTOs.CompanyParticipants;
 using API.DTOs.EmployeeParticipants;
 using API.DTOs.EventPayments;
-using API.Utilities.Enums;
 using System.ComponentModel.DataAnnotations;
 
 namespace API.DTOs.Events;
 
 public class GetMasterEventDto
 {
-    [Required]
-    public Guid EventGuid { get; set; }
+    public Guid Guid { get; set; }
 
     [Required]
-    public string EventName { get; set; }
+    public string Name { get; set; }
     public string? Thumbnail { get; set; }
 
     [Required]
     public string Description { get; set; }
 
     [Required]
-    public bool IsPublished { get; set; }
-
-    [Required]
-    public bool IsPaid { get; set; }
-
-    [Required]
-    public decimal Price { get; set; }
+    public string Visibility { get; set; }
 
     [Required]
     public string Category { get; set; }
 
     [Required]
-    public EventStatus Status { get; set; }
+    public string PlaceType { get; set; }
 
     [Required]
-    public DateTime StartDate { get; set; }
+    public string Payment { get; set; }
 
     [Required]
-    public DateTime EndDate { get; set; }
+    public decimal Price { get; set; }
 
     [Required]
     public int Quota { get; set; }
+    public int Joined { get; set; }
 
     [Required]
-    public int UsedQuota { get; set; }
+    public string StartDate { get; set; }
+
+    [Required]
+    public string EndDate { get; set; }
+
+    [Required]
+    public string Organizer { get; set; }
 
     [Required]
     public string Place { get; set; }
 
     [Required]
-    public bool IsActive { get; set; }
+    public string PublicationStatus { get; set; }
 
-    [Required]
-    public Guid CreatedBy { get; set; }
-
-    public string? CompanyName { get; set; }
+    public Guid? PaymentGuid { get; set; }
 
     public List<CompanyParticipantsDto>? CompanyParticipants { get; set; }
     public List<EmployeeParticipantsDto>? EmployeeParticipants { get; set; }
