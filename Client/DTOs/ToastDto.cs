65ce57a2d86d0afbf854f4f39e34d440e5b95841
AHN-YU-JIN\Ahn Yu Jin <dev.rohmatstar@gmail.com>
2023-08-01 12:22:11 +0700

    UI Adjustment, Toast Added

---
+++ Client/DTOs/ToastDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Client.DTOs
+{
+    public class ToastDto
+    {
+        public string? Color{ get; set; }
+        public string? Title { get; set; }
+        public string? Subtitle { get; set; }
+    }
+}
