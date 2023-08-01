ecd35f7d94b3de65282aaaf3c21df090b590262e
AHN-YU-JIN\Ahn Yu Jin <dev.rohmatstar@gmail.com>
2023-08-01 12:20:55 +0700

    Restyle Authentication

---
+++ Client/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using Client.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Client.Controllers
+{
+    public class AuthController : Controller
+    {
+        /*[Authorize]*/
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        /*[Authorize]*/
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        /*[Authorize]*/
+        public IActionResult ForgetPassword()
+        {
+            return View();
+        }
+
+        /*[Authorize]*/
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
