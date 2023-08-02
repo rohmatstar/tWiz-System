27d75a0542569b6feeb97b22ddf9c9d234111110
Febrianto752 <febrianto.bekasi@gmail.com>
2023-08-02 10:55:56 +0700

    fix: update json content data

--- API/Repositories/EmployeeRepository.cs
+++ API/Repositories/EmployeeRepository.cs
@@ -1,8 +1,6 @@
 ﻿using API.Contracts;
 using API.Data;
-using API.DTOs.Events;
 using API.Models;
-using System.Xml.Linq;
 
 namespace API.Repositories;
 
@@ -17,9 +15,9 @@ public class EmployeeRepository : GeneralRepository<Employee>, IEmployeeReposito
         return _context.Set<Employee>().Where(employee => employee.FullName.Contains(name));
     }
 
-    public IEnumerable<Employee> GetName(Guid fkAccountGuid)
-    {
-        return _context.Set<Employee>().Where(employee => employee.AccountGuid == fkAccountGuid);
-    }
+    //public IEnumerable<Employee> GetName(Guid fkAccountGuid)
+    //{
+    //    return _context.Set<Employee>().Where(employee => employee.AccountGuid == fkAccountGuid);
+    //}
 
 }
