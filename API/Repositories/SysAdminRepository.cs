235364104427a4e284ea84ada0c19ae78dddbcf5
Febrianto752 <febrianto.bekasi@gmail.com>
2023-08-02 15:47:53 +0700

    feat: add contract sysadmin repo
    Add interface sysadmin and sysadmin repo. Update account service get email sysadmin

---
+++ API/Repositories/SysAdminRepository.cs
@@ -0,0 +1,13 @@
+﻿using API.Contracts;
+using API.Data;
+using API.Models;
+
+namespace API.Repositories;
+
+public class SysAdminRepository : GeneralRepository<SysAdmin>, ISysAdminRepository
+{
+    public SysAdminRepository(TwizDbContext context) : base(context)
+    {
+    }
+}
+
