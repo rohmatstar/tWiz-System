18459d2909a876db00dd355c0c8aca84bc7f5ad0
Febrianto752 <febrianto.bekasi@gmail.com>
2023-08-05 16:40:17 +0700

    feat: update event payment repository
    Add deletes and creates method

--- API/Repositories/CompanyParticipantRepository.cs
+++ API/Repositories/CompanyParticipantRepository.cs
@@ -12,36 +12,31 @@ public class CompanyParticipantRepository : GeneralRepository<CompanyParticipant
 
     public bool Deletes(List<CompanyParticipant> companyParticipants)
     {
-        var transaction = _context.Database.BeginTransaction();
 
         try
         {
             _context.Set<CompanyParticipant>().RemoveRange(companyParticipants);
             _context.SaveChanges();
-            transaction.Commit();
+
             return true;
         }
         catch
         {
-            transaction.Rollback();
+
             return false;
         }
     }
 
     public bool Creates(List<CompanyParticipant> companyParticipants)
     {
-        var transaction = _context.Database.BeginTransaction();
-
         try
         {
             _context.Set<CompanyParticipant>().AddRange(companyParticipants);
             _context.SaveChanges();
-            transaction.Commit();
             return true;
         }
         catch
         {
-            transaction.Rollback();
             return false;
         }
     }
