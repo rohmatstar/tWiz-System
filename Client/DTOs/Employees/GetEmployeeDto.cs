7cc5fe9e0ebf141ade75a3a056bd1b2319321bcf
AHN-YU-JIN\Ahn Yu Jin <dev.rohmatstar@gmail.com>
2023-08-06 02:07:42 +0700

    Import Employee DTO from API

---
+++ Client/DTOs/Employees/GetEmployeeDto.cs
@@ -0,0 +1,36 @@
+﻿using Client.Utilities.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Client.DTOs.Employees
+{
+    public class GetEmployeeDto
+    {
+        [Required]
+        public Guid Guid { get; set; }
+
+        [Required]
+        public string Nik { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        public GenderEnum Gender { get; set; }
+
+        [Required]
+        public DateTime HiringDate { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [Phone]
+        public Guid CompanyGuid { get; set; }
+
+        [Required]
+        public Guid AccountGuid { get; set; }
+    }
+}
