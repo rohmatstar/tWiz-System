97006671181e7a2db8fc667e5f94cc41dea86e4b
Febrianto752 <febrianto.bekasi@gmail.com>
2023-08-07 06:50:01 +0700

    feat: add get participants paid event dto

---
+++ API/DTOs/EventPayments/GetCompanyParticipantsPaidEventDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs.EventPayments;
+
+public class GetCompanyParticipantsPaidEventDto
+{
+    [Required]
+    public Guid ParticipantGuid { get; set; }
+
+    public string? EventName { get; set; }
+
+    [Required]
+    public Guid CompanyGuid { get; set; }
+
+    public string CompanyName { get; set; }
+
+    public string InvitationStatus { get; set; }
+
+    public bool IsPresent { get; set; }
+
+    public Guid EventPayment { get; set; }
+    public string PaymentImageUrl { get; set; }
+}
+
---
+++ API/DTOs/EventPayments/GetParticipantsPaidEvent.cs
@@ -0,0 +1,16 @@
+﻿using Client.Views.EventPayment;
+
+namespace API.DTOs.EventPayments;
+
+public class GetParticipantsPaidEvent
+{
+    public Guid EventGuid { get; set; }
+    public string EventName { get; set; }
+    public Guid MakerGuid { get; set; }
+    public string MakerName { get; set; }
+
+    public List<GetEmployeeParticipantsPaidEventDto> EmployeeParticipantsPaidEvent { get; set; }
+    public List<GetCompanyParticipantsPaidEventDto> CompanyParticipantsPaidEvent { get; set; }
+
+}
+
